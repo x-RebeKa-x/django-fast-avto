7e83c11867a254597b38d1a3c100e18587e3a011
Satana <ken83897@gmail.com>
2026-01-26 21:06:28 +0300

    add car and car_category

--- store/cars/admin.py
+++ store/cars/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from cars.models import Car, CarCategory
+
+admin.site.register(Car)
+admin.site.register(CarCategory)
--- store/cars/models.py
+++ store/cars/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class CarCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    # img = models.ImageField(upload_to='cars/')
+    description = models.TextField()
+    short_description = models.TextField(max_length=64, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    category = models.ForeignKey(CarCategory, on_delete=models.PROTECT) # нельзя удалить категорию пока не удалить все товары связанные с ним
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
