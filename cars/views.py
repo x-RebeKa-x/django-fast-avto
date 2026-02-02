520230579b2ab525537af11ed6cf966cde7a7d39
Satana <ken83897@gmail.com>
2026-02-02 18:42:36 +0300

    add category_table, for: categories and car, json and comment

--- cars/views.py
+++ cars/views.py
@@ -8,6 +8,7 @@ def index(request):
     return render(request,"cars/index.html")
 
 def cars(request):
+    # Этот context нужен для карточек товара и отображения всех категорий, которые находятся в базе данных
     context = {
         'cars': Car.objects.all(),
         'categories': CarCategory.objects.all(),
