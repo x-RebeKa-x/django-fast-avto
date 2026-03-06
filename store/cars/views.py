1dae1b9e9cb870207a3173f89654303b36a1f40e
Satana <ken83897@gmail.com>
2026-03-06 19:32:12 +0300

    block time_end in page_booking

--- store/cars/views.py
+++ store/cars/views.py
@@ -1,6 +1,7 @@
 from lib2to3.fixes.fix_input import context
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from cars.models import CarCategory, Car, Basket
@@ -158,8 +159,6 @@ def payment_page(request, car_id):
     if session_data and isinstance(session_data, dict):
         booking_data.update(session_data)
 
-    time_valid = True
-
     if not session_data and request.method == 'POST':
         address = request.POST.get('address')
         date = request.POST.get('date')
@@ -173,50 +172,41 @@ def payment_page(request, car_id):
             time_start_object = datetime.strptime(time, '%H:%M').time()
             time_end_object = datetime.strptime(time_end, '%H:%M').time()
 
-            if date_start_object == date_end_object and time_end_object <= time_start_object:
-                time_valid = False
-                booking_data = {
-                    'address': address,
-                    'date': date,
-                    'time': time,
-                    'date_end': date_end,
-                    'time_end': time_end,
-                }
-                messages.error(request, 'Время окончания должно быть позже времени получения')
+            if date_start_object == date_end_object and time_end_object < time_start_object:
+                raise ValidationError({'time_end_object': 'Время окончания не должно быть раньше начала'})
+
+            days = (date_end_object - date_start_object).days
+            if days < 1:
+                days = 1
+
+            if days % 10 == 1 and days % 100 != 11:
+                days_word = 'день'
+            elif 2 <= days % 10 <= 4 and not (12 <= days % 100 <= 14):
+                days_word = 'дня'
             else:
-                days = (date_end_object - date_start_object).days
-                if days < 1:
-                    days = 1
+                days_word = 'дней'
 
-                if days % 10 == 1 and days % 100 != 11:
-                    days_word = 'день'
-                elif 2 <= days % 10 <= 4 and not (12 <= days % 100 <= 14):
-                    days_word = 'дня'
-                else:
-                    days_word = 'дней'
+            total_price = car.price * days
 
-                total_price = car.price * days
+            booking_data = {
+                'car_id': car.id,
+                'car_name': car.name,
+                'car_price': str(car.price),
+                'address': address,
+                'date': date,
+                'time': time,
+                'date_end': date_end,
+                'time_end': time_end,
+                'days': days,
+                'days_word': days_word,
+                'total_price': str(total_price),
+            }
 
-                booking_data = {
-                    'car_id': car.id,
-                    'car_name': car.name,
-                    'car_price': str(car.price),
-                    'address': address,
-                    'date': date,
-                    'time': time,
-                    'date_end': date_end,
-                    'time_end': time_end,
-                    'days': days,
-                    'days_word': days_word,
-                    'total_price': str(total_price),
-                }
-
-                request.session['booking_data'] = booking_data
+            request.session['booking_data'] = booking_data
 
     context = {
         'car': car,
         'booking': booking_data,
-        'time_valid': time_valid,
     }
 
     return render(request, "cars/payment_page.html", context)
